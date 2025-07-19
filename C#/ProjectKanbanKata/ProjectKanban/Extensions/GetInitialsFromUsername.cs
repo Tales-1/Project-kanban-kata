57b75c799747b15bb8f7b35b821a57dd41108166
Jawad Nazir <Jawad.nazir124@gmail.com>
2025-07-19 16:13:45 +0100

    GetInitialsFromUsername extension.

---
+++ C#/ProjectKanbanKata/ProjectKanban/Extensions/GetInitialsFromUsername.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace ProjectKanban.Extensions;
+
+public static class StringExtensions
+{
+    public static string GetInitialsFromUsername(this string username)
+    {
+        if(string.IsNullOrEmpty(username))
+            return string.Empty;
+
+        // splitting and looping over names allows to account for users with multiple names
+        string[] names = username.Split(" ");
+
+        StringBuilder initialsBuilder = new ();
+
+        foreach (string name in names)
+        {
+            initialsBuilder.Append(name[0]);
+        }
+
+        return initialsBuilder.ToString();
+    }
+}
